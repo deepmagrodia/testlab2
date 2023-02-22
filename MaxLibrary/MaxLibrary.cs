09e1e215e706375e806b173b7005d9cded7b2284
deepmagrodia <deepmagrodia1496@email.com>
2023-02-22 12:47:36 -0500

    First Commit

---
+++ MaxLibrary/MaxLibrary.cs
@@ -0,0 +1,20 @@
+﻿/* Deep Magrodia
+   Dr Yulia Kumar 
+   Tech 4982 - 01
+   Lab #2 */
+using System;
+namespace MaxNumberLib
+{
+    public class MaxNumber
+    {
+        public static double FindMaxTwo(double num1, double num2)
+        {
+            return Math.Max(num1, num2);
+        }
+
+        public static double FindMaxThree(double num1, double num2, double num3)
+        {
+            return Math.Max(Math.Max(num1, num2), num3);
+        }
+    }
+}
---
+++ MaxTest/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Deep Magrodia
+   Dr Yulia Kumar 
+   Tech 4982 - 01
+   Lab #2 */
+
+using System;
+using MaxNumberLib;
+
+namespace MaxTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First number :");
+                double num1=double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second number :");
+                double num2 = double.Parse(Console.ReadLine());
+             
+            double max = MaxNumber.FindMaxTwo(num1, num2);
+            Console.WriteLine("The maximum of {0} and {1} is {2}", num1, num2, max);
+            Console.WriteLine("Enter the Third number :");
+            double num3 = double.Parse(Console.ReadLine());
+
+            double max2 = MaxNumber.FindMaxThree(num1, num2, num3);
+            Console.WriteLine("The maximum of {0} {1} {2} is {3}", num1, num2, num3, max2);
+
+
+
+        }
+    }
+}
